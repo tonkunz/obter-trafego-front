82fbfbf8a3a9002deb5fa659396b29260bd76087
Everton Kunz <ehokeverton@gmail.com>
2022-03-26 18:53:13 -0300

    Remoção dos componentes não utilizados:  - Messages;  - Notifications;  - Shortcuts;

--- src/app/layout/layouts/empty/empty.component.ts
+++ src/app/layout/layouts/empty/empty.component.ts
@@ -2,32 +2,16 @@ import { Component, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { Subject } from 'rxjs';
 
 @Component({
-    selector     : 'empty-layout',
-    templateUrl  : './empty.component.html',
-    encapsulation: ViewEncapsulation.None
+  selector: 'empty-layout',
+  templateUrl: './empty.component.html',
+  encapsulation: ViewEncapsulation.None,
 })
-export class EmptyLayoutComponent implements OnDestroy
-{
-    private _unsubscribeAll: Subject<any> = new Subject<any>();
+export class EmptyLayoutComponent implements OnDestroy {
+  private _unsubscribeAll: Subject<any> = new Subject<any>();
 
-    /**
-     * Constructor
-     */
-    constructor()
-    {
-    }
-
-    // -----------------------------------------------------------------------------------------------------
-    // @ Lifecycle hooks
-    // -----------------------------------------------------------------------------------------------------
-
-    /**
-     * On destroy
-     */
-    ngOnDestroy(): void
-    {
-        // Unsubscribe from all subscriptions
-        this._unsubscribeAll.next(null);
-        this._unsubscribeAll.complete();
-    }
+  ngOnDestroy(): void {
+    // Unsubscribe from all subscriptions
+    this._unsubscribeAll.next(null);
+    this._unsubscribeAll.complete();
+  }
 }
--- src/app/layout/layouts/vertical/classic/classic.module.ts
+++ src/app/layout/layouts/vertical/classic/classic.module.ts
@@ -9,10 +9,6 @@ import { FuseFullscreenModule } from '@fuse/components/fullscreen';
 import { FuseLoadingBarModule } from '@fuse/components/loading-bar';
 import { FuseNavigationModule } from '@fuse/components/navigation';
 import { LanguagesModule } from 'app/layout/common/languages/languages.module';
-import { MessagesModule } from 'app/layout/common/messages/messages.module';
-import { NotificationsModule } from 'app/layout/common/notifications/notifications.module';
-import { SearchModule } from 'app/layout/common/search/search.module';
-import { ShortcutsModule } from 'app/layout/common/shortcuts/shortcuts.module';
 import { UserModule } from 'app/layout/common/user/user.module';
 import { SharedModule } from 'app/shared/shared.module';
 import { ClassicLayoutComponent } from 'app/layout/layouts/vertical/classic/classic.component';
@@ -30,10 +26,6 @@ import { ClassicLayoutComponent } from 'app/layout/layouts/vertical/classic/clas
     FuseLoadingBarModule,
     FuseNavigationModule,
     LanguagesModule,
-    MessagesModule,
-    NotificationsModule,
-    SearchModule,
-    ShortcutsModule,
     UserModule,
     SharedModule,
   ],
